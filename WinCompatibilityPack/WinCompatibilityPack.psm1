7cb57d1160dac94f305724c5ef0d35644deacda6
Mark Kraus <github@markekraus.com>
2018-05-31 15:52:52 -0500

    Assign operation output to null

--- WinCompatibilityPack/WinCompatibilityPack.psm1
+++ WinCompatibilityPack/WinCompatibilityPack.psm1
@@ -485,7 +485,7 @@ function Import-WinModule
             { 
                 $module = Import-Module -Name $name -NoClobber @importModuleParameters 
                 # Hack using private reflection to keep the proxy module from shadowing the real module.
-                $module.
+                $null = $module.
                     GetType().
                         GetMethod("SetName",[System.Reflection.BindingFlags]'Instance, NonPublic').
                             Invoke($module, @($module.Name + ".WinModule"))
